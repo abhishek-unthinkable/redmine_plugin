2c83d7b101cae8d860a102a2c6cffbaf43743262
Abhishek Kushwaha <abhishek.kushwaha@unthinkable.co>
2020-09-09 14:04:04 +0530

    rubocop pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 polls = Poll.create([
-    {question: "Can you see this poll"},
-    {question: "Can you see this other poll"}
-])
+                      { question: "Can you see this poll" },
+                      { question: "Can you see this other poll" }
+                    ])
